b2deb877601a10693d0bc7e395cbefa84bf63941
AllisonGrace77 <146791496+AllisonGrace77@users.noreply.github.com>
2024-05-02 19:21:12 -0700

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -1,15 +1,9 @@
 var table = prompt("Enter an integer between 0 and 10");             // Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = '<h2>Multiplication Table</h2>';// Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
+if (operator === 'multiplication') {
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
