e01dc54be2bb298b3b11c1187c2610b551bc2718
AllisonGrace77 <146791496+AllisonGrace77@users.noreply.github.com>
2024-05-08 19:03:02 -0700

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -1,9 +1,3 @@
-	
-// var rHeight =5;
-// var colorEven = "orange";
-// var colorOdd = "black";
-// var symbol ="*";
-
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 up(pHeight, pColorEven, pColorOdd, pSymbol);
 down(pHeight, pColorEven, pColorOdd, pSymbol);
