fc6d9464049140c53303262478611cd9c96af232
AllisonGrace77 <146791496+AllisonGrace77@users.noreply.github.com>
2024-05-08 21:08:54 -0700

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -11,19 +11,15 @@ function up(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=1;i<=pHeight;i++){
 	rLine +="<p>";
-	//Create each line on the Rhombus
-	for(j=0;j<pHeight;j++){
-		if(j<(pHeight-i))
-		//should the symbol be white
-			rLine +="<span style='color: #ffffff;'>" + pSymbol +"</span>";
-		
-		//Is the position even or odd so we change the color
-		else if (j%2)
-		//even
-			rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-		else
-		//odd
-			rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+	if(i%2===0){
+		var num = Math.ceil((pHeight-i)/2);
+	}
+	else {
+		rLine+="<span style='color: #ffffff;'> ' ' </span>"
+		var num = Math.floor((pHeight-i)/2);
+	}
+	for(j=0;j<num;j++){
+		rLine +="<span style='color: #ffffff;'>" + pSymbol + "</span>";
 		}
 	for(k=0;k<i;k++){
 		if (k%2)
@@ -34,28 +30,25 @@ for (i=1;i<=pHeight;i++){
 			rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
 		}
 	rLine +="</p>\n";
-	//console.log(rLine);
+	//console.log(rLine);*/
 }
 document.getElementById("up").innerHTML = rLine;
 }
 
 function down(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
-for (i=pHeight;i > 0;i--){
+for (i=pHeight-1;i > 0;i--){
 	rLine +="<p>";
-	//Create each line on the Rhombus
-	for(j=0;j<pHeight;j++){
-		if(j<(pHeight-i))
-		//should the symbol be white
-			rLine +="<span style='color: #ffffff ;'>" + pSymbol +"</span>";
-		
-		//Is the position even or odd so we change the color
-		else if (j%2)
-		//even
-			rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-		else
-		//odd
-			rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+	
+	if(i%2===0){
+		var num = Math.ceil((pHeight-i)/2);
+	}
+	else {
+		rLine+="<span style='color: #ffffff;'> ' ' </span>"
+		var num = Math.floor((pHeight-i)/2);
+	}
+	for(j=0;j<num;j++){
+		rLine +="<span style='color: #ffffff;'>" + pSymbol + "</span>";
 		}
 	for(k=0;k<i;k++){
 		//Is the position even or odd so we change the color
